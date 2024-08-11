fea81144c6869fd8fa4174f351b455bd1d1aceb9
MaksimBin <130301328+MaksimBin@users.noreply.github.com>
2024-08-12 02:27:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var elem = document.documentElement;
 
 function openFullscreen() {
@@ -22,11 +23,17 @@ let getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
 
 setInterval(() => {
   document.querySelector('.app').style = `
-  box-shadow: ${getRandomArbitrary(-40, 40)}px 0 25px 1px rgb(139, 0, 0);
+  box-shadow: ${getRandomArbitrary(-40, 40)}px 0 25px 1px red;
   transform: rotate(${getRandomArbitrary(-360, 360)}deg);
   `
   document.querySelector('.app2').style = `
     box-shadow: 15px ${getRandomArbitrary(-20, 20)}px 25px 1px rgb(255, 20, 147);
+    transform: rotate(${getRandomArbitrary(-360, 360)}deg);
+    `
+   
+    document.querySelector('.app3').style = `
+    border-radius: ${getRandomArbitrary(-50, 50)}%;
+    box-shadow: 0px 0px ${getRandomArbitrary(-50, 50)}px ${getRandomArbitrary(-50, 50)}px red;
     transform: rotate(${getRandomArbitrary(-360, 360)}deg);
     `
 }, 3000)
@@ -64,26 +71,36 @@ const onCangeInput = (valueCange) => {
   rengeAudio = Number(valueCange)
 }
 
+let boolPause
+let audioCurentTime
 
 let interval
 
 const audioPlay = () => {
+  
 
   if (alertAudio()) {
     return
   }
-
-  ren.min = 0
-
-  audio.duration = 0
-
-  audio.src = playLists[numberTrack].url
+  
+  if (!boolPause) {
+    ren.min = 0
+    audio.duration = 0
+  }
+  
+  audio = new Audio(playLists[numberTrack].url)
+ 
+ if (boolPause) {
+   audio.currentTime = audioCurentTime
+   console.log(audio.currentTime, audioCurentTime)
+ }
 
   document.querySelector('.PlayPause').innerHTML = ` <div style="transform: rotate(90deg);" onclick="stopAudio()">	&#61;</div>`
-
-
+  
   audio.play()
-
+  
+  onload(audio)
+  
   interval = setInterval(() => {
 
     rengeAudio = rengeAudio + 1
@@ -91,31 +108,108 @@ const audioPlay = () => {
     ren.value = rengeAudio
 
     ren.max = getMax(audio.duration)
+  
+   audioCurentTime = audio.currentTime
 
 
     document.querySelector('.time').innerHTML = toTime(parseInt(audio.duration) - parseInt(audio.currentTime))
-
-
+    
+    
     if (toTime(parseInt(audio.duration) - parseInt(audio.currentTime)) == "00:00:00") {
       nextAudio()
     }
 
   }, 1000)
+  
 
+}
+
+const openEculizer = () => {
+  document.querySelector('.eculizer-content').style = 'left: 0;'
+}
+
+const closeEculizer = () => {
+  document.querySelector('.eculizer-content').style = '-500p'
+}
+
+let FILSTYLE = 'black'
+let SHADOW = 'deeppink'
+
+var analyzer, canvas, ctx
+
+const onload = (audio) => {
+  canvas = document.getElementById('canvas')
+  canvas.width = window.innerWidth
+  canvas.height = 250
+  ctx = canvas.getContext('2d')
+  var audioContext = new AudioContext()
+  analyzer = audioContext.createAnalyser()
+  analyzer.fftSize = 2048
+  var source = audioContext.createMediaElementSource(audio)
+  source.connect(analyzer)
+  analyzer.connect(audioContext.destination)
+
+  draw()
+}
+
+function draw() {
+  requestAnimationFrame(draw)
+  var spectrum = new Uint8Array(analyzer.frequencyBinCount)
+  analyzer.getByteFrequencyData(spectrum)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  var prev = {
+    x: 0,
+    y: 0
+  }
+
+  var w = 1
+
+  ctx.beginPath()
+  ctx.moveTo(0, canvas.height)
+  ctx.lineTo(0, canvas.height - spectrum[0])
+
+  for (var i = 0; i < spectrum.length; i += w) {
+
+    var curr = {
+      x: i,
+      y: canvas.height - spectrum[i]
+    }
+
+    var next = {
+      x: i + w,
+      y: canvas.height - spectrum[i + w]
+    }
+
+    var xc = (curr.x + next.x) / 2;
+    var yc = (curr.y + next.y) / 2;
+    ctx.quadraticCurveTo(curr.x, curr.y, xc, yc)
+
+    prev = {
+      x: curr.x,
+      y: curr.y
+    }
+  }
+
+  ctx.quadraticCurveTo(prev.x, prev.y, canvas.width, canvas.height)
+  ctx.fillStyle = FILSTYLE;
+  ctx.closePath(); //draw to first point
+  ctx.shadowColor = SHADOW;
+  ctx.shadowBlur = 50;
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = -5;
+  ctx.fill();
 }
 
 const stopAudio = () => {
 
   clearInterval(interval)
-
-  rengeAudio = 0
-
-  audio.duration = 0
-  ren.max = 0
-
+ 
   document.querySelector('.PlayPause').innerHTML = ` <div onclick="audioPlay()">	&#10148;</div>`
 
   audio.pause()
+  
+  boolPause = true
 
 }
 
@@ -142,12 +236,12 @@ const nextAudio = () => {
 
   audio.duration = 0
   ren.max = 0
+  
+  boolPause = false
 
   audioPlay()
 
 }
-
-
 
 const backAudio = () => {
 
@@ -172,6 +266,8 @@ const backAudio = () => {
 
   audio.duration = 0
   ren.max = 0
+  
+  boolPause = false
 
   audioPlay()
 
@@ -189,7 +285,6 @@ const getMax = (dur) => {
 
 }
 
-
 const openModal = () => {
 
   tach()
@@ -200,8 +295,6 @@ const openModal = () => {
 
   document.querySelector('.modal').style = "display:flex;"
 
-
-
   document.querySelector('.mapList').innerHTML = playLists.map((x, index) => `
 
 <div class="list" style="box-shadow: 0 5px 10px -5px ${getBoxColorList(numberTrack, index)};" onclick="plyaList(${index})">
@@ -211,7 +304,7 @@ const openModal = () => {
     
   
   <div class="textListCenter">
-      <div class="h">${x.name}</div>
+      <div class="h ${getAnimationClass(numberTrack, index)}">${getMinText(x.name)}...</div>
       <p>Исполнитель</p>
   </div>
 
@@ -244,7 +337,7 @@ const plyaList = (indx) => {
       
     
     <div class="textListCenter">
-        <div class="h">${x.name}</div>
+        <div class="h ${getAnimationClass(numberTrack, index)}">${getMinText(x.name)}...</div>
         <p>Исполнитель</p>
     </div>
   
@@ -294,3 +387,20 @@ const getStateAudio = (treckIndex, indexList) => {
     return "musika"
   }
 }
+
+let getAnimationClass = (treckIndex, indexList) => {
+
+  if (treckIndex == indexList) {
+    return "animationText"
+  }
+
+  return
+}
+
+
+let getMinText = (text) => {
+return  text.slice(0, 35)
+}
+
+
+
